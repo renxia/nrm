4438a7972bad8c33d03989ebe86455628afac73d
renxia <lzwy0820@qq.com>
2023-03-28 14:14:03 +0800

    test: remove coffee

--- tests/cli.test.js
+++ tests/cli.test.js
@@ -1,8 +1,23 @@
-const coffee = require("coffee");
 // const open = require("open");
+const { spawnSync: spawn } = require("node:child_process");
 const { color } = require("console-log-colors");
+const { Request } = require("@lzwme/fe-utils");
 const { onHome, onTest } = require("../src/actions.js");
 const isWin = process.platform === "win32";
+const isCI = Boolean(process.env.CI || process.env.GITHUB_CI);
+const pkgName = "@lzwme/nrm";
+
+jest.spyOn(Request.prototype, "get").mockImplementation((url) => {
+  return new Promise((resolve) => {
+    setTimeout(
+      () =>
+        resolve({
+          response: { statusCode: url.includes("error.com") ? -1 : 200 },
+        }),
+      (Math.random() + 1) * 1000
+    );
+  });
+});
 
 process.setMaxListeners(10000);
 jest.setTimeout(20000);
@@ -10,75 +25,55 @@ jest.setTimeout(20000);
 //   default: jest.fn(() => console.log("browser opened"))
 // }));
 
-jest.mock("@lzwme/fe-utils", () => {
-  return {
-    color: require("console-log-colors"),
-    Request: {
-      getInstance: () => ({
-        get: jest.fn((url) => {
-          return new Promise((resolve) => {
-            setTimeout(
-              () => resolve({ response: { statusCode: url.includes("error.com") ? -1 : 200 } }),
-              (Math.random() + 1) * 1000
-            );
-          });
-        }),
-      }),
-    },
-  };
-});
+let __NRM_VERSION__ = null;
 
 beforeAll(async () => {
-  const { stdout } = await coffee.spawn("nrm", ["-V"], { shell: isWin }).end();
-  __NRM_VERSION__ = stdout ? stdout : null;
-  await coffee.spawn("npm", ["link"], { shell: isWin }).end();
+  if (!isCI) {
+    const { stdout } = spawn("nrm", ["-V"], { shell: isWin, encoding: "utf8" });
+    __NRM_VERSION__ = stdout || null;
+  }
+
+  spawn("npm", ["link"], { shell: isWin, encoding: "utf8" });
 });
 
 afterAll(async () => {
-  await coffee.spawn("npm", ["unlink nrm -g"], { shell: isWin }).end();
+  await spawn("npm", [`unlink ${pkgName} -g`], {
+    shell: isWin,
+    encoding: "utf8",
+  });
   if (__NRM_VERSION__ !== null) {
-    await coffee
-      .spawn("npm", [`install -g nrm@${__NRM_VERSION__}`], { shell: isWin })
-      .end();
+    await spawn("npm", [`install -g ${pkgName}@${__NRM_VERSION__}`], {
+      shell: isWin,
+      encoding: "utf8",
+    });
   }
 });
 
 it("nrm ls", async () => {
-  await coffee
-    .spawn("nrm", ["use cnpm"], { shell: isWin })
-    .expect("stdout", /The registry has been changed to 'cnpm'/g)
-    .expect("code", 0)
-    .end();
+  let r = await spawn("nrm", ["use cnpm"], { shell: isWin, encoding: "utf8" });
+  expect(/The registry has been changed to 'cnpm'/g.test(r.stdout)).toBe(true);
+  expect(r.status).toBe(0);
 
-  const { stdout, code } = await coffee
-    .spawn("nrm", ["ls"], { shell: isWin })
-    .end();
-
+  const { stdout } = await spawn("nrm", ["ls"], {
+    shell: isWin,
+    encoding: "utf8",
+  });
   const match = color.green.bold("* ") + "cnpm";
   expect(stdout.includes(match)).toBe(true);
-  expect(code).toBe(0);
 });
 
 it("nrm use <registry>", async () => {
-  await coffee
-    .spawn("nrm", ["use cnpm"], { shell: isWin })
-    .expect("stdout", /The registry has been changed to 'cnpm'/g)
-    .expect("code", 0)
-    .end();
+  const r = spawn("nrm", ["use cnpm"], { shell: isWin, encoding: "utf8" });
+  expect(/The registry has been changed to 'cnpm'/g.test(r.stdout)).toBe(true);
+  expect(r.status).toBe(0);
 });
 
 it("nrm current", async () => {
-  await coffee
-    .spawn("nrm", ["use cnpm"], { shell: isWin })
-    .expect("stdout", /The registry has been changed to 'cnpm'/g)
-    .expect("code", 0)
-    .end();
+  let r = spawn("nrm", ["use cnpm"], { shell: isWin, encoding: "utf8" });
+  expect(/The registry has been changed to 'cnpm'/g.test(r.stdout)).toBe(true);
 
-  await coffee
-    .spawn("nrm", ["current"], { shell: isWin })
-    .expect("stdout", /cnpm/g)
-    .expect("code", 0)
-    .end();
+  r = spawn("nrm", ["current"], { shell: isWin, encoding: "utf8" });
+  expect(/cnpm/g.test(r.stdout)).toBe(true);
 });
 
 describe("nrm command which needs to add a custom registry", () => {
@@ -89,19 +84,19 @@ describe("nrm command which needs to add a custom registry", () => {
     /* the globalVariable in jest.config.js */
     __REGISTRY__ = customName;
 
-    await coffee
-      .spawn("nrm", [`add ${__REGISTRY__} ${url}`], { shell: isWin })
-      .expect("stdout", /success/g)
-      .expect("code", 0)
-      .end();
+    const r = spawn("nrm", [`add ${__REGISTRY__} ${url}`], {
+      shell: isWin,
+      encoding: "utf8",
+    });
+    expect(r.stdout.includes("success")).toBe(true);
   });
 
   afterEach(async () => {
-    await coffee
-      .spawn("nrm", [`del ${__REGISTRY__}`], { shell: isWin })
-      .expect("stdout", /has been deleted successfully/g)
-      .expect("code", 0)
-      .end();
+    const r = spawn("nrm", [`del ${__REGISTRY__}`], {
+      shell: isWin,
+      encoding: "utf8",
+    });
+    expect(/has been deleted successfully/g.test(r.stdout)).toBe(true);
   });
 
   it("nrm rename", async () => {
@@ -112,24 +107,22 @@ describe("nrm command which needs to add a custom registry", () => {
       "g"
     );
 
-    await coffee
-      .spawn("nrm", [`rename ${customName} ${newName}`], { shell: isWin })
-      .expect("stdout", match)
-      .expect("code", 0)
-      .end();
+    const r = spawn("nrm", [`rename ${customName} ${newName}`], {
+      shell: isWin,
+      encoding: "utf8",
+    });
+    expect(match.test(r.stdout)).toBe(true);
   });
 
   it("nrm set <name>", async () => {
     const attr = "attr";
     const value = "value";
 
-    await coffee
-      .spawn("nrm", [`set ${__REGISTRY__} -a ${attr} -v ${value}`], {
-        shell: isWin,
-      })
-      .expect("stdout", /successfully/g)
-      .expect("code", 0)
-      .end();
+    const r = spawn("nrm", [`set ${__REGISTRY__} -a ${attr} -v ${value}`], {
+      shell: isWin,
+      encoding: "utf8",
+    });
+    expect(/successfully/g.test(r.stdout)).toBe(true);
   });
 
   it("nrm test [registry]", async () => {
@@ -143,17 +136,17 @@ describe("nrm command which needs to add a custom registry", () => {
     const scopeName = "nrm";
     const url = "https://scope.example.org";
 
-    await coffee
-      .spawn("nrm", [`set-scope ${scopeName} ${url}`], { shell: isWin })
-      .expect("stdout", /success/g)
-      .expect("code", 0)
-      .end();
+    let r = spawn("nrm", [`set-scope ${scopeName} ${url}`], {
+      shell: isWin,
+      encoding: "utf8",
+    });
+    expect(/success/g.test(r.stdout)).toBe(true);
 
-    await coffee
-      .spawn("nrm", [`del-scope ${scopeName}`], { shell: isWin })
-      .expect("stdout", /success/g)
-      .expect("code", 0)
-      .end();
+    r = spawn("nrm", [`del-scope ${scopeName}`], {
+      shell: isWin,
+      encoding: "utf8",
+    });
+    expect(/success/g.test(r.stdout)).toBe(true);
   });
 
   it("nrm set-hosted-repo <name> <repo>", async () => {
@@ -163,34 +156,38 @@ describe("nrm command which needs to add a custom registry", () => {
       "g"
     );
 
-    await coffee
-      .spawn("nrm", [`set-hosted-repo ${__REGISTRY__} ${repo}`], {
-        shell: isWin,
-      })
-      .expect("stdout", match)
-      .expect("code", 0)
-      .end();
+    const r = spawn("nrm", [`set-hosted-repo ${__REGISTRY__} ${repo}`], {
+      shell: isWin,
+      encoding: "utf8",
+    });
+    expect(match.test(r.stdout)).toBe(true);
   });
 
   it("login <name> [base64]", async () => {
     const username = "username";
     const password = "password";
 
-    await coffee
-      .spawn("nrm", [`login ${__REGISTRY__} -u ${username} -p ${password}`], {
+    let r = spawn(
+      "nrm",
+      [`login ${__REGISTRY__} -u ${username} -p ${password}`],
+      {
         shell: isWin,
-      })
-      .expect("stdout", /success/g)
-      .expect("code", 0)
-      .end();
+        encoding: "utf8",
+      }
+    );
+    expect(/success/g.test(r.stdout)).toBe(true);
 
-    await coffee
-      .spawn("nrm", [`login ${__REGISTRY__}`], { shell: isWin })
-      .expect(
-        "stderr",
-        /Authorization information in base64 format or username & password is required/g
+    r = spawn("nrm", [`login ${__REGISTRY__}`], {
+      shell: isWin,
+      encoding: "utf8",
+    });
+
+    console.log("---stderr: ", r.stderr);
+    expect(
+      /Authorization information in base64 format or username & password is required/g.test(
+        r.stderr
       )
-      .end();
+    ).toBe(true);
   });
 });
 
